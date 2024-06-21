3a98829b815c91c59a5da5faf08899c20aab9ac9
Deepuverma07 <deepuverma6725@gmail.com>
2024-06-21 14:55:23 +0530

    hood design done

---
+++ www/main.js
@@ -0,0 +1,16 @@
+
+
+// /effect on bounce on the ask me anything
+$(document).ready(function () {
+    
+    $('.text').textillate({
+        loop: true,
+        sync: true,
+        in:{
+            effect: "bounceIn",
+        },
+        out:{
+            effect: "bounceOut",
+        },
+    })
+});
